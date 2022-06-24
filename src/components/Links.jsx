7545839c02b42effeecd70964f1f71bc7f9541a7
Nathaniel Patterson <nathanielpattersongit@gmail.com>
2022-06-24 01:36:06 -0400

    making components DRY

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,54 +1,42 @@
-import { Button, Paper } from "@mui/material";
+import { Button, Paper, Stack } from "@mui/material";
+
+const links = [
+  {
+    text: "Carnival.com",
+    link: "https://www.carnival.com",
+  },
+  {
+    text: "Arise Okta",
+    link: "https://arise.okta.com/",
+  },
+  {
+    text: "Easy Peasy Carnival Breezy",
+    link: "https://docs.google.com/spreadsheets/d/1q5CFh0hfJx2GcwUwP9ByVOp-abzbz9LGoM2BBMACyiQ/edit#gid=661269468",
+  },
+  {
+    text: "Have Fun. Be Safe.",
+    link: "https://www.carnival.com/Legal/covid-19-legal-notices/covid-19-guest-protocols?icid=advisory_cruisehealth_040122",
+  },
+];
 
 export default function Links() {
   return (
-    <>
-      <Paper style={{ padding: 8 }}>
-        <Button
-          variant="p"
-          component="p"
-          align="center"
-          onClick={() => {
-            window.open("https://www.carnival.com");
-          }}
-        >
-          Carnival
-        </Button>
-        <Button
-          variant="p"
-          component="p"
-          align="center"
-          onClick={() => {
-            window.open("https://arise.okta.com/");
-          }}
-        >
-          Arise
-        </Button>
-        <Button
-          variant="p"
-          component="p"
-          align="center"
-          onClick={() => {
-            window.open(
-              "https://docs.google.com/spreadsheets/d/1q5CFh0hfJx2GcwUwP9ByVOp-abzbz9LGoM2BBMACyiQ/edit#gid=661269468"
-            );
-          }}
-        >
-          Easy Breezy KB
-        </Button>
-        <Button
-          variant="p"
-          component="p"
-          align="center"
-          onClick={() => {
-            window.open(
-              "https://www.carnival.com/Legal/covid-19-legal-notices/covid-19-guest-protocols?icid=advisory_cruisehealth_040122"
-            );
-          }}
-        >
-          Have Fun. Be Safe.
-        </Button>
-      </Paper>
-    </>
+    <Paper style={{ padding: 8, overflow: "auto", height: 250 }} elevation={5}>
+      <Stack direction="row" sx={{ flexWrap: "wrap" }}>
+        {links.map((link) => (
+          <Button
+            sx={{ margin: 1 }}
+            variant="contained"
+            component="p"
+            align="center"
+            onClick={() => {
+              window.open(link.link);
+            }}
+          >
+            {link.text}
+          </Button>
+        ))}
+      </Stack>
+    </Paper>
   );
 }
--- src/features/callFlow/components/OpeningScript.jsx
+++ src/features/callFlow/components/OpeningScript.jsx
@@ -1,21 +1,20 @@
 import { Typography, Paper } from "@mui/material";
 
+const script = [
+  "Thanks for calling Carnival, my name is ______. Who do I have the pleasure of speaking with?",
+  "Thank you,___ TA/DG's_NAME___. May I please have your booking number?",
+  "** Proceed to pull up booking in Navigator.**",
+  "** While waiting on NAVI, can ask how your day is going etc. **",
+];
+
 export default function OpeningScript() {
   return (
-    <Paper style={{ margin: "5%" }}>
-      <Typography margin>
-        Thanks for calling Carnival, my name is ______. Who do I have the
-        pleasure of speaking with?
-      </Typography>
-      <Typography margin>
-        Thank you,___ TA/DG's_NAME___. May I please have your booking number?
-      </Typography>
-      <Typography margin>
-        ** Proceed to pull up booking in Navigator.**
-      </Typography>
-      <Typography margin>
-        ** While waiting on NAVI, can ask how your day is going etc. **
-      </Typography>
+    <Paper style={{ margin: "4%" }} elevation={10}>
+      {script.map((item) => (
+        <Typography margin padding>
+          {item}
+        </Typography>
+      ))}
     </Paper>
   );
 }
