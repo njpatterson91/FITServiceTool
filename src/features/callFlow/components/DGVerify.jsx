cf81cf523c14f01d7221705fd703737b0abf09f6
Nathaniel Patterson <nathanielpattersongit@gmail.com>
2022-06-26 22:47:19 -0400

    started call verify refactor to make it data driven and not hardcoded

--- src/features/callFlow/components/DGVerify.jsx
+++ src/features/callFlow/components/DGVerify.jsx
@@ -1,95 +1,58 @@
-import { Container, Grid, Typography, Button, Paper } from "@mui/material";
+import { Container, Grid, Typography, Button } from "@mui/material";
 import { useState } from "react";
-import OnBooking from "../../../components/OnBooking";
-import VerifyButton from "../../../components/VerifyButton";
-
-const verifyFlow = {
-  onBooking: {
-    nestSteps: {
-      question: "Is the caller on the booking or the Direct Info",
-      button1: {
-        text: "On Booking",
-        nextSteps: {
-          question: "Did they provide the Booking Number",
-          button1: {
-            text: "Yes",
-            nextSteps: {
-              question: "Did they provide the pin",
-              button1: {
-                text: "Yes",
-                nextSteps: {
-                  button1: {
-                    text: "verify",
-                  },
-                },
-              },
-              button2: {
-                text: "No",
-                question:
-                  "Caller provides: Name of ship and date of sailing, and their full name and date of birth.",
-              },
-            },
-          },
-          button2: {
-            text: "No",
-            nextSteps: {
-              question:
-                "Caller needs to provide: Ship and Sail date, one name and dob, PIN#. You Provide Booking#",
-            },
-          },
-        },
-      },
-      button2: {
-        text: "Direct Info",
-        nextSteps: {
-          question:
-            "Caller needs to provide: Booking#, Ship and Sail Date, PIN#. If the caller is missing the pin ALL Names and DOB",
-        },
-      },
-    },
-  },
-  notOnBooking: {},
-};
+import { useSetRecoilState, useRecoilState } from "recoil";
+import { verified, stage } from "../../../store/atoms";
+const verifyFlow = require("../../../json/verifyFlow.json");
 
 export default function DGVerify() {
-  const [onBooking, setOnBooking] = useState();
+  const [step, setStep] = useState(verifyFlow.start);
+  const [currentStage, setStage] = useRecoilState(stage);
+  const verifyCaller = useSetRecoilState(verified);
   return (
     <Container maxWidth="sm" style={{ overflow: "auto", height: 250 }}>
       <Typography variant="p" component="p" align="center" marginTop={5}>
-        Is the caller on the booking or the Direct Info
+        {step.question}
       </Typography>
 
       <Grid container spacing={2} align="center" marginTop={3}>
-        <Grid item xs={6}>
-          <Button
-            onClick={() => {
-              setOnBooking(true);
-            }}
-          >
-            Yes
-          </Button>
-        </Grid>
-        <Grid item xs={6}>
-          <Button
-            onClick={() => {
-              setOnBooking(false);
-            }}
-          >
-            No
-          </Button>
-        </Grid>
+        {step.verify === false && (
+          <>
+            {" "}
+            <Grid item xs={6}>
+              <Button
+                onClick={() => {
+                  setStep(step.button1.nextSteps);
+                  console.log(step);
+                }}
+              >
+                {step.button1.text}
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button
+                onClick={() => {
+                  setStep(step.button2.nextSteps);
+                  console.log(step);
+                }}
+              >
+                {step.button2.text}
+              </Button>
+            </Grid>{" "}
+          </>
+        )}
+        {step.verify === true && (
+          <Grid item xs={12}>
+            <Button
+              onClick={() => {
+                verifyCaller(true);
+                setStage(currentStage + 1);
+              }}
+            >
+              Verify
+            </Button>
+          </Grid>
+        )}
       </Grid>
-      {onBooking === true && <OnBooking />}
-      {onBooking === false && (
-        <Container maxWidth="sm" align="center">
-          <Typography variant="p" component="p" align="center" marginTop={5}>
-            You must do a full verification. Booking#, Ship and Sail date, PIN#.
-            If caller is missing pin# verify all names and dates of birth
-          </Typography>
-
-          <VerifyButton />
-        </Container>
-      )}
     </Container>
   );
 }
