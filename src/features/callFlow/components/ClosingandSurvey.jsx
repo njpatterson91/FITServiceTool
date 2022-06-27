070191e7c8a16fc2ad46511dbaeabe68b5611827
Nathaniel Patterson <nathanielpattersongit@gmail.com>
2022-06-27 03:56:57 -0400

    styling changes

--- src/features/callFlow/components/ClosingandSurvey.jsx
+++ src/features/callFlow/components/ClosingandSurvey.jsx
@@ -15,38 +15,52 @@ export default function ClosingandSurvey() {
             }}
           >
             <Typography>
-              ** Please select Travel Advisor or Direct Guest from booking type
-              to view appropriate scripting. **
+              Please select Travel Advisor or Direct Guest from booking type to
+              view appropriate scripting.
             </Typography>
           </div>
         </Paper>
       )}
       {customer.bookingType === "TA" && (
-        <Paper style={{ margin: "4%", padding: "2%" }} elevation={10}>
-          <Typography>
-            It was a pleasure assisting you, can you please remain on this line
-            for a brief survey about my service on this call? Have a FUn-TASTIC
-            day!
-          </Typography>
+        <Paper style={{ margin: "4%", padding: "3px" }} elevation={10}>
+          <div
+            style={{
+              backgroundColor: "green",
+              borderRadius: "5px",
+            }}
+          >
+            <Typography>
+              It was a pleasure assisting you, can you please remain on this
+              line for a brief survey about my service on this call? Have a
+              FUn-TASTIC day!
+            </Typography>
+          </div>
         </Paper>
       )}
       {customer.bookingType === "DG" && (
-        <Paper style={{ margin: "4%", padding: "2%" }} elevation={10}>
-          <Typography>
-            As a thank you for calling, we've arranged for you to receive an
-            exciting discount offer from Hilton Grand Vacations which includes
-            $200 towards a future carnival cruise. May i transfer you for those
-            brief details
-          </Typography>
-          <Typography>
-            If yes Transfer call to 70710 and remain on the line until the
-            prompt that advises that the guest has been connected.
-          </Typography>
-          <Typography>
-            If no say It was a pleasure assisting you, can you please remain on
-            this line for a brief survey about my service on this call? Have a
-            FUn-TASTIC day!
-          </Typography>
+        <Paper style={{ margin: "4%", padding: "3px" }} elevation={10}>
+          <div
+            style={{
+              backgroundColor: "green",
+              borderRadius: "5px",
+            }}
+          >
+            <Typography>
+              As a thank you for calling, we've arranged for you to receive an
+              exciting discount offer from Hilton Grand Vacations which includes
+              $200 towards a future carnival cruise. May i transfer you for
+              those brief details
+            </Typography>
+            <Typography>
+              If yes Transfer call to 70710 and remain on the line until the
+              prompt that advises that the guest has been connected.
+            </Typography>
+            <Typography>
+              If no say It was a pleasure assisting you, can you please remain
+              on this line for a brief survey about my service on this call?
+              Have a FUn-TASTIC day!
+            </Typography>
+          </div>
         </Paper>
       )}
     </>
